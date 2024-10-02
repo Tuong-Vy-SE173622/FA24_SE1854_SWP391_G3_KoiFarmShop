f2584badb6c5caf1384749c257130809fb49fcf2
Hoang-Trung-01 <TrungHSE160786@fpt.edu.vn>
2024-10-02 23:51:22 +0700

    add backend base

---
+++ Sourcecode/Back-end/KoiFarmShop.APIService/Program.cs
@@ -0,0 +1,41 @@
+
+using KoiFarmShop.Data;
+using KoiFarmShop.Data.Models;
+
+namespace KoiFarmShop.APIService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //builder.Services.AddScoped<FA_SE1854_SWP391_G3_KoiFarmShopContext>();
+            builder.Services.AddScoped<UnitOfWork>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Sourcecode/Back-end/KoiFarmShop.Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using KoiFarmShop.Data.Models;
+using KoiFarmShop.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.Data
+{
+    public class UnitOfWork
+    {
+        private FA_SE1854_SWP391_G3_KoiFarmShopContext _context;
+        private KoiRepository _koiRepository;
+        private KoiTypeRepositories _koiTypeRepositories;
+
+        public UnitOfWork() => _context ??= new FA_SE1854_SWP391_G3_KoiFarmShopContext();
+
+        public KoiRepository KoiRepository
+        {
+            get
+            {
+                return _koiRepository ??= new KoiRepository(_context);
+            }
+        }
+    }
+}

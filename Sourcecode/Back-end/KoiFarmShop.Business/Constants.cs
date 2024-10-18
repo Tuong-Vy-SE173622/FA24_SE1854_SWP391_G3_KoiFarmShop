e4df9d8a4fdbc155075b57ee90ccaf37ba434b9e
somethinn <irtsomethinn@gmail.com>
2024-10-18 22:28:52 +0700

    Add static class Constants

---
+++ Sourcecode/Back-end/KoiFarmShop.Business/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.Business
+{
+    public static class Constants
+    {
+        public const double VAT = 0.10;    
+    }
+}

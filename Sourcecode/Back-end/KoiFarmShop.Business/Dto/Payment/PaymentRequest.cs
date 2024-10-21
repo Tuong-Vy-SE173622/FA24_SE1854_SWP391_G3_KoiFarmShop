053f76f2cb920b25830e74a761b672894f35d4b8
somethinn <irtsomethinn@gmail.com>
2024-10-21 22:02:02 +0700

    implement VnPay for payment service

---
+++ Sourcecode/Back-end/KoiFarmShop.Business/Dto/Payment/PaymentRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoiFarmShop.Business.Dto.Payment
+{
+    public class PaymentRequest
+    {
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string OrderDescription { get; set; }
+
+        [Required]
+        public string OrderId { get; set; }
+
+        public string? BankCode { get; set; }
+    }
+}
+
---
+++ Sourcecode/Back-end/KoiFarmShop.Business/Dto/Payment/PaymentResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace KoiFarmShop.Business.Dto.Payment
+{
+    public class PaymentResponse
+    {
+        public bool checkSignature { get; set; }
+        public bool checkTmnCode { get; set; }
+        public string ResponseCode { get; set; }
+    }
+}

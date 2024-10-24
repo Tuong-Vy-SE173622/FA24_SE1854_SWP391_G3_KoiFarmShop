32ee6080e533f2fa73b7a6c1c1a34bb1bd88b4cd
somethinn <irtsomethinn@gmail.com>
2024-10-24 22:13:15 +0700

    modify ResultDto methods to quickly generate

--- Sourcecode/Back-end/KoiFarmShop.Business/Dto/ResultDto.cs
+++ Sourcecode/Back-end/KoiFarmShop.Business/Dto/ResultDto.cs
@@ -1,4 +1,7 @@
-﻿namespace KoiFarmShop.Business.Dto
+﻿using Microsoft.Identity.Client;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace KoiFarmShop.Business.Dto
 {
     public class ResultDto
     {
@@ -6,5 +9,39 @@
         public int Code { get; set; }
         public object? Data { get; set; }
         public string? Message { get; set; }
+
+        public void success()
+        {
+            IsSuccess = true;
+            Code = 400;
+            Data = null;
+            Message = "Success";
+        }
+        public void success(object data)
+        {
+            IsSuccess = true;
+            Code = 400;
+            Data = data;
+            Message = "Success";
+        }
+
+        public void success(object data, string message) 
+        {
+            IsSuccess = true;
+            Code = 400;
+            Data = data;
+            Message = message;
+        }
+
+        public void error(int code, string message)
+        {
+            IsSuccess = false;
+            Code = code;
+            Data = null;
+            Message = message;
+        }
+        
     }
+
+    
 }

5d860192778b39c34ca45b3cf3e75772f969aedc
vominhman <manvmse172292@fpt.edu.vn>
2024-11-02 14:27:23 +0700

    update field startdate enddate of promotion, isactive

--- Sourcecode/Back-end/KoiFarmShop.Business/Business/PromotionBusiness/IPromotionService.cs
+++ Sourcecode/Back-end/KoiFarmShop.Business/Business/PromotionBusiness/IPromotionService.cs
@@ -7,7 +7,7 @@ namespace KoiFarmShop.Business.Business.PromotionBusiness
     public interface IPromotionService
     {
         public Task<IEnumerable<PromotionDto>> GetAllPromotionsAsync();
-        public Task<ResultDto> GetPromotionList(int? promotionId);
+        Task<ResultDto> GetPromotionList(bool? isActive);
         //public Task<int> CreatePromotionAsync(PromotionCreateDto promotionCreateDto);
         public Task<ResultDto> AddNewPromotion(PromotionCreateDto model, ClaimsPrincipal userCreate);
         public Task<ResultDto> UpdatePromotion(int promotionId, PromotionCreateDto promotionDto, ClaimsPrincipal userUpdate);

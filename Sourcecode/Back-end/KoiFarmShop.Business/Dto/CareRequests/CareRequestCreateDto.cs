addc2bbb707456d2686cee15090aeb1e405296d6
Hoang-Trung-01 <TrungHSE160786@fpt.edu.vn>
2024-11-21 20:26:09 +0700

    clean up

--- Sourcecode/Back-end/KoiFarmShop.Business/Dto/CareRequests/CareRequestCreateDto.cs
+++ Sourcecode/Back-end/KoiFarmShop.Business/Dto/CareRequests/CareRequestCreateDto.cs
@@ -16,7 +16,5 @@ namespace KoiFarmShop.Business.Dto.CareRequests
         public int? CarePlanId { get; set; }
 
         public string? Note { get; set; }
-
-        public string? CreatedBy { get; set; }
     }
 }

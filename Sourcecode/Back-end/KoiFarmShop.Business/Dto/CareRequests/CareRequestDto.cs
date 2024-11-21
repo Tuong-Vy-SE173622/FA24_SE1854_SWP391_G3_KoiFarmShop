4ad78280bc113a569225740bd3236c9384ae60f0
somethinn <irtsomethinn@gmail.com>
2024-11-21 21:29:43 +0700

    clean code

--- Sourcecode/Back-end/KoiFarmShop.Business/Dto/CareRequests/CareRequestDto.cs
+++ Sourcecode/Back-end/KoiFarmShop.Business/Dto/CareRequests/CareRequestDto.cs
@@ -13,30 +13,6 @@ namespace KoiFarmShop.Business.Dto.CareRequests
 {
     public class CareRequestDto
     {
-        //public int RequestId { get; set; }
-
-        //public int? CustomerId { get; set; }
-
-        //public int? KoiId { get; set; }
-
-        //public DateTime? RequestedDate { get; set; }
-
-        //public string? Status { get; set; }
-
-        //public bool? IsActive { get; set; }
-
-        //public string? Note { get; set; }
-
-        //public DateTime? CreatedAt { get; set; }
-
-        //public string CreatedBy { get; set; }
-
-        //public DateTime? UpdatedAt { get; set; }
-
-        //public string UpdatedBy { get; set; }
-        //public CustomerDto? Customer { get; set; }
-
-        //public List<CareRequestDetailDto> CareRequestDetailsDto { get; set; }
         public int CareRequestId { get; set; }
 
         public int? CustomerId { get; set; }
